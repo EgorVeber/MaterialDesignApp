6f2c94826107c12ee5c0aacb71003c5d17616ae5
JamesOhara <egor.veber.02.07.95@gmail.com>
2022-06-17 15:18:33 +0300

    Приступает к ДЗ. Делаем другой вариант меню(простой)

--- app/src/main/java/ru/gb/veber/materialdesignapp/view/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/ru/gb/veber/materialdesignapp/view/BottomNavigationDrawerFragment.kt
@@ -42,6 +42,7 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
 
             when (menuItem.itemId) {
+
                 R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
                 R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
                 R.id.switch_item -> {
